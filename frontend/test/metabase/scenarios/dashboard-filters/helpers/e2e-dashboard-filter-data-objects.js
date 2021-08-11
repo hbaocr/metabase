c587fbbeef920f0041fcfbfd1cfa12e9f5571d72
Nemanja <31325167+nemanjaglumac@users.noreply.github.com>
2021-08-11 23:52:56 +0200

    Add initial set of tests around SQL dashboard location filters

--- frontend/test/metabase/scenarios/dashboard-filters/helpers/e2e-dashboard-filter-data-objects.js
+++ frontend/test/metabase/scenarios/dashboard-filters/helpers/e2e-dashboard-filter-data-objects.js
@@ -145,3 +145,36 @@ export const DASHBOARD_SQL_TEXT_FILTERS = {
     representativeResult: "Small Marble Shoes",
   },
 };
+
+export const DASHBOARD_SQL_LOCATION_FILTERS = {
+  Dropdown: {
+    sqlFilter: "string/=",
+    value: "Rye",
+    representativeResult: "Arnold Adams",
+  },
+  "Is not": {
+    sqlFilter: "string/!=",
+    value: "Rye",
+    representativeResult: "Hudson Borer",
+  },
+  Contains: {
+    sqlFilter: "string/contains",
+    value: "oo",
+    representativeResult: "Hudson Borer",
+  },
+  "Does not contain": {
+    sqlFilter: "string/does-not-contain",
+    value: "oo",
+    representativeResult: "Domenica Williamson",
+  },
+  "Starts with": {
+    sqlFilter: "string/starts-with",
+    value: "W",
+    representativeResult: "Hudson Borer",
+  },
+  "Ends with": {
+    sqlFilter: "string/ends-with",
+    value: "g",
+    representativeResult: "Aracely Jenkins",
+  },
+};
